1b7369fda64bc6edc49e271b9871c342d6396422
joshcom <joshua.murray@gmail.com>
2011-06-24 14:01:29 -0500

    Couple'a TODOs

--- lib/hello_goodbye/consoles/manager_console.rb
+++ lib/hello_goodbye/consoles/manager_console.rb
@@ -4,9 +4,13 @@ module HelloGoodbye
       case command
       when /^start/
         # TODO: Grab the ID, and start the foreman.
+        send_response :success => true,
+          :message => "start will be implemented soon"
         return true
       when /^stop /
         # TODO: Grab the ID, and stop the foreman.
+        send_response :success => true,
+          :message => "stop will be implemented soon"
         return true
       when "foremen"
         send_response :success => true, :message => "ok", 
