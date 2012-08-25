5fbf49698a52e3826feb999f5df4494ecd43f198
Joshua Murray <joshua.murray@gmail.com>
2012-08-25 07:04:31 -0500

    Fixed not-the-best-cp-job-ever

--- Rakefile
+++ Rakefile
@@ -1,2 +1,10 @@
 #!/usr/bin/env rake
 require "bundler/gem_tasks"
+
+require 'rspec/core/rake_task'
+
+desc "Run specs"
+RSpec::Core::RakeTask.new(:spec) do |t| 
+    t.rspec_opts = ["-c", "-f progress","-r ./spec/spec_helper.rb"]
+      t.pattern = './spec/**/*_spec.rb'
+end
--- lib/hello_goodbye.rb
+++ lib/hello_goodbye.rb
@@ -1,5 +1,25 @@
-require "hello_goodbye/version"
+require 'rubygems'
+require 'eventmachine'
+require 'json'
+
+require File.expand_path('../hello_goodbye/foremen_manager',__FILE__)
 
 module HelloGoodbye
-  # Your code goes here...
+
+  # Resets the current foremen manager, so that the next time
+  # self.manager is called, a new ForemenManager instance will be
+  # created.
+  def self.reset!
+    @manager = nil
+  end
+
+  # Create a new manager or use the existing manager.  If an existing manager exists,
+  # port and server will be ignored.
+  # Parameters:
+  # * port:   The port the manager should connect to.
+  # * server: The server the service will run from.
+  def self.manager(port=ForemenManager.default_manager_port,server=Foreman.default_server)
+    @manager ||= ForemenManager.new(:port => port, :server => server)
+  end
+
 end
